35b77a9ed67e9d9b221743a04953110ac83a9d47
walter velazco <waltervelazco@chaco.gov.ar>
2026-08-26 18:29:27 -0300

    Maquetacion

---
+++ Presentacion/Modelo/CtlModelo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP_ControlVehicular.Presentacion.Modelo
+{
+    /// <summary>
+    /// Lógica de interacción para CtlModelo.xaml
+    /// </summary>
+    public partial class CtlModelo : UserControl
+    {
+        public CtlModelo()
+        {
+            InitializeComponent();
+        }
+    }
+}
